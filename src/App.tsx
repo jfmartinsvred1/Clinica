20c5fc1a89a15c765f606b61a06e3c592fedf18b
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-09-22 22:34:04 -0300

    sidebar open and close

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { FilterLeft } from 'react-bootstrap-icons'
 import './App.css'
 import SideBar from './components/sidebar'
+import { useState } from 'react'
 
 function App() {
+  const [valueLeftSidebar, setValueLeftSideBar]=useState("left-[-300px]");
 
   return (
-        <SideBar/>
+    <>
+      <span className='absolute text-white text-4xl top-5 left-4 cursor-pointer' onClick={()=>setValueLeftSideBar("")}>
+                <FilterLeft className='px-2 bg-gray-900 rounded-md'/>
+      </span>
+      <SideBar valueLeftSideBar={valueLeftSidebar} setValueLeftSideBar={setValueLeftSideBar}/>
+    </>
   )
 }
 
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,13 +1,17 @@
-import { AppIndicator, House, List, Plus, X } from 'react-bootstrap-icons'
+import { AppIndicator, FilterLeft, List, PersonCircle, Plus, X } from 'react-bootstrap-icons'
 import './sidebar.css'
-const SideBar = () => {
+type SideBarProps={
+    valueLeftSideBar:string,
+    setValueLeftSideBar:(a:string)=>void
+}
+const SideBar = ({valueLeftSideBar,setValueLeftSideBar}:SideBarProps) => {
     return (
-        <div className='sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900'>
+        <div className={'sidebar fixed top-0 bottom-0 lg:left-0 p-2  w-[300px] overflow-y-auto text-center bg-gray-900 '+valueLeftSideBar}>
           <div className='text-gray-200 text-xl'>
             <div className='p-2.5 mt-1 flex items-center'>
-              <AppIndicator className='px-2 py-1 bg bg-sky-700 rounded-md'/>
+              <AppIndicator width="32px" className='px-2 py-1 bg-sky-700 rounded-md'/>
               <h1 className="font-bold">Clínica Lorem</h1>
-              <X className='cursor-pointer lg:hidden'/>
+              <X width="32px" className='cursor-pointer ml-20 lg:hidden' onClick={()=>setValueLeftSideBar("left-[-300px]")}/>
             </div>
             <hr className='my-2 text-gray-600'/>
           </div>
@@ -27,6 +31,11 @@ const SideBar = () => {
           <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-sky-700 text-white '>
             <List className='text-sm' />
             <span className='text-[14px] ml-4 text-gray-200'>Médicos</span>
+          </div>
+          <hr className='my-2 text-gray-600'/>
+          <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 text-white '>
+            <PersonCircle className='text-sm' />
+            <span className='text-[14px] ml-4 text-gray-200 cursor-pointer hover:text-sky-400'>Username</span>
           </div>
         </div>
     )
