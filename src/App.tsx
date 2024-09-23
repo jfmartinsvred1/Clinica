49beb0352002acff92478047b978f2ec3d8fb396
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-09-23 00:53:30 -0300

    main content

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,30 @@ import { FilterLeft } from 'react-bootstrap-icons'
 import './App.css'
 import SideBar from './components/sidebar'
 import { useState } from 'react'
+import Footer from './components/footer';
 
 function App() {
-  const [valueLeftSidebar, setValueLeftSideBar]=useState("left-[-300px]");
+  const [valueLeftSidebar, setValueLeftSideBar] = useState("left-[-300px]");
+  const [valueClassMain, setValueClassMain] = useState("")
+  function openSidebar() {
+    setValueLeftSideBar("")
+    setValueClassMain("left-[300px] ")
+  }
+  function closeSideBar() {
+    setValueLeftSideBar("left-[-300px]")
+    setValueClassMain("")
+  }
 
   return (
     <>
-      <span className='absolute text-white text-4xl top-5 left-4 cursor-pointer' onClick={()=>setValueLeftSideBar("")}>
-                <FilterLeft className='px-2 bg-gray-900 rounded-md'/>
+      <span className='absolute text-white text-4xl top-5 left-4 cursor-pointer' onClick={() => openSidebar()}>
+        <FilterLeft className='px-2 bg-gray-900 rounded-md' />
       </span>
-      <SideBar valueLeftSideBar={valueLeftSidebar} setValueLeftSideBar={setValueLeftSideBar}/>
+      <SideBar valueLeftSideBar={valueLeftSidebar} closeSideBar={closeSideBar} />
+      <div className={`relative top-[64px] h-[calc(100vh-64px)] lg:w-[calc(100%-300px)] ${valueClassMain===""?"w-100%":"w-[calc(100%-300px)]"} w-full ${valueClassMain}`}>
+        <Footer valueClassMain={valueClassMain} />
+      </div>
+
     </>
   )
 }
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,15 @@
-const Footer=()=>{
-    return(
-        <div className="bg-sky-900 h-20">
+type FooterProps = {
+    valueClassMain: string;
+};
+
+const Footer = ({ valueClassMain }: FooterProps) => {
+    return (
+        <div className={`absolute bg-red-500 w-full flex justify-center items-center bottom-0 h-10`}>
+            <h1>
+                Desenvolvido por <a target="_blank" rel="noopener noreferrer" href="https://jfmartins.vercel.app/">Jfmartins</a>
+            </h1>
         </div>
-    )
-}
-export default Footer
+    );
+};
+
+export default Footer;
