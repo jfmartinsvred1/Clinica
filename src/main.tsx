83ce4d97467f7dbe69b3b2793c95709d9a1ea6a4
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-09-25 21:13:49 -0300

    reactDom

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-    <App/>
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
